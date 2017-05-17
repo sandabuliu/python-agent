b7642ff752be599f9cc334275d1dd876aba5ffb1
tongbin <tongbin@haizhi.com>
2017-05-17 11:52:04 +0800

    添加 blaze_agent 注册

--- agent/__init__.py
+++ agent/__init__.py
@@ -7,5 +7,10 @@ from agent.agent.agent import Agent
 from agent.agent import source, output, sender
 from agent.config import rule, ruletocfg, rulebase
 
+try:
+    from blaze_agent import register
+    register(uri='agent:.+', priority=12)
+except ImportError:
+    pass
 
 __author__ = 'tong'
